ce8c2e7078a3e444b3d15e2f131a4c66affd8e4e
Wilfried Kopp <wilfried@parity.io>
2023-08-22 11:36:46 +0200

    fmt

--- src/version.rs
+++ src/version.rs
@@ -27,18 +27,18 @@ pub use semver::Version;
 ///
 /// Return the new version.
 pub fn bump_for_breaking_change(version: Version) -> Version {
-    let mut new_version = version;
+	let mut new_version = version;
 
-    if new_version.pre != semver::Prerelease::EMPTY {
-        // Remove pre-release tag like `-dev` if present
-        new_version.pre = semver::Prerelease::EMPTY;
-    } else if new_version.major == 0 {
-        // Else, bump minor if 0.x.0 crate
-        new_version.minor += 1;
-    } else {
-        // Else bump major version
-        new_version.major += 1;
-    }
+	if new_version.pre != semver::Prerelease::EMPTY {
+		// Remove pre-release tag like `-dev` if present
+		new_version.pre = semver::Prerelease::EMPTY;
+	} else if new_version.major == 0 {
+		// Else, bump minor if 0.x.0 crate
+		new_version.minor += 1;
+	} else {
+		// Else bump major version
+		new_version.major += 1;
+	}
 
-    new_version
+	new_version
 }
